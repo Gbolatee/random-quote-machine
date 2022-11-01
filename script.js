b982f64b1f750ddbe2d4fef0c7158dac789f6b24
Gbolatee <109198562+Gbolatee@users.noreply.github.com>
2022-11-01 03:40:31 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+const quoteLists = [
+  `Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa`,
+
+  `When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt`,
+
+  `Always remember that you are absolutely unique. Just like everyone else -Margaret Mead`,
+
+  `Don't judge each day by the harvest you reap but by the seeds that you plant.-Robert Louis Stevenson`,
+
+  `The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt`,
+
+  `Tell me and I forget. Teach me and I remember. Involve me and I learn. -Benjamin Franklin`,
+
+  `The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller`,
+
+  `It is during our darkest moments that we must focus to see the light. -Aristotle`,
+
+  `Whoever is happy will make others happy too.-Anne Frank`,
+
+  `Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson`,
+];
+
+console.log(quoteLists);
+document.querySelector(".write").textContent = quoteLists[7];
+document.querySelector(".new").addEventListener("click", function () {
+  let result = Math.trunc(Math.random() * quoteLists.length);
+  document.querySelector(".write").textContent = quoteLists[result];
+});
+// for (let i = 0; i < quoteLists.length; i++)
+//   document.querySelector(".new").addEventListener("click", function () {
+//     document.querySelector(".write").textContent = quoteLists[i];
+//   });
